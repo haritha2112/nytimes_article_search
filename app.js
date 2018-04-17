002ad838db8be16ddde6c01b5d557f371a65e446
Haritha Rathinakumar <hrathin@g.clemson.edu>
2018-04-17 10:02:49 -0400

    Include a function that adds articles from NYtimes API

--- app.js
+++ app.js
@@ -28,19 +28,34 @@
     });
 
     function addImage() {
-        let htmlContent = '';
-        const data = JSON.parse(this.responseText);
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
 
-        if (data && data.results && data.results[0]) {
-            const firstImage = data.results[0];
-            htmlContent = `<figure>
-                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-            </figure>`;
-        } else {
-            htmlContent = '<div class="error-no-image">No images available</div>';
-        }
+      if (data && data.results && data.results[0]) {
+          const firstImage = data.results[0];
+          htmlContent = `<figure>
+              <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+              <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+          </figure>`;
+      } else {
+          htmlContent = '<div class="error-no-image">No images available</div>';
+      }
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
 
-        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    function addArticles() {
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+
+      if (data.response && data.response.docs && data.response.docs.length > 1) {
+        htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article"> 
+            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+          </li>`
+        ).join('') + '</ul>';
+      } else {
+        htmlContent = '<div class="error-no-articles">No articles available</div>';
+      }
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
 })();
