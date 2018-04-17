8da342c757d3a792fb447f1f2f53edbd307b45b6
Haritha Rathinakumar <hrathin@g.clemson.edu>
2018-04-17 09:49:23 -0400

    Add an AJAX request to get data from unsplash.com

--- app.js
+++ app.js
@@ -8,5 +8,15 @@
       e.preventDefault();
       responseContainer.innerHTML = '';
       searchedForText = searchField.value;
+      
+      const imgRequest = new XMLHttpRequest();
+      imgRequest.onload = addImage;
+      imgRequest.onerror = function (err) {
+          requestError(err, 'image');
+      };
+      imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+      imgRequest.setRequestHeader('Authorization', 'Client-ID b65dff736c9af6a5cf8bd401ee5d94b6cc83e19562e71bba71d8b004a8da7051');
+      imgRequest.send();
     });
+
 })();
